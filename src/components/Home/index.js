2b7f11f7eaf5fac170a40b66d5c7bfebf4351e25
B S Mounika <67068850+mounika-01@users.noreply.github.com>
2025-05-14 14:58:34 +0530

    Update index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,31 +1,38 @@
+// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+
 import Navbar from '../Navbar'
 
-import {ThemeContext} from '../../context/ThemeContext'
 import './index.css'
 
 const Home = () => (
   <ThemeContext.Consumer>
     {value => {
       const {isDarkTheme} = value
+
+      const homeBgClassName = isDarkTheme ? 'home-bg-dark' : 'home-bg-light'
+
+      const homeImageURL = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
+
+      const homeTextClassName = isDarkTheme
+        ? 'home-text-light'
+        : 'home-text-dark'
+
       return (
-        <div
-          className={`main-home-container ${isDarkTheme ? 'dark' : 'light'}`}
-        >
-           <img
-   src={
-     isDarkTheme
-       ? 'https://assets.ccbp.in/frontend/react-js/home-dark-img.png'
-       : 'https://assets.ccbp.in/frontend/react-js/home-light-img.png'
-   }
-   className="home-image"
-   alt="home" // Ensure this matches exactly
- />
-          <h1 className={`home-heading-${isDarkTheme ? 'dark' : 'light'}`}>
-            Home
-          </h1>
+        <div className={`home-app-container ${homeBgClassName}`}>
+          <Navbar />
+          <div className="home-responsive-container">
+            <div className="home-container">
+              <img className="home-image" src={homeImageURL} alt="home" />
+              <h1 className={`home-heading ${homeTextClassName}`}>Home</h1>
+            </div>
+          </div>
         </div>
       )
     }}
   </ThemeContext.Consumer>
 )
+
 export default Home
